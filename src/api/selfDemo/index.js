648c06c83a5811f7bbd632eb953a6e7c5373ff99
TZZ-WIN11\Administrator <tzzhmmcc@163.com>
2026-03-13 21:34:38 +0800

    feat: 增加空格

--- src/api/selfDemo/index.js
+++ src/api/selfDemo/index.js
@@ -1,4 +1,4 @@
-import service from '@/utils/request';
+﻿import service from '@/utils/request';
 
 // selfDemo 模块的接口从这里统一收口。
 // 后续需要具体接口时，直接基于 service.get/post/put/delete 继续补充即可。
@@ -19,3 +19,4 @@ export function UploadFileToBase64(data) {
 //     responseType: "blob",
 //   });
 // }
+
